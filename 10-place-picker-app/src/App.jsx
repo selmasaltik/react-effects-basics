c2bd2fea56655ffa164480c74393578b83cfdcac
selmasaltik <selmasaltiik@gmail.com>
2024-10-05 17:29:02 +0300

    useEffect Not Needed: Another Example

--- 10-place-picker-app/src/App.jsx
+++ 10-place-picker-app/src/App.jsx
@@ -7,20 +7,16 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) => 
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
-
-  useEffect(() => {
-    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-    const storedPlaces = storedIds.map((id) => 
-      AVAILABLE_PLACES.find((place) => place.id === id)
-    );
-
-    setPickedPlaces(storedPlaces);
-  }, []);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -52,7 +48,6 @@ function App() {
       return [place, ...prevPickedPlaces];
     });
 
-    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     if (storedIds.indexOf(id) === -1) {
       localStorage.setItem(
         'selectedPlaces', 
@@ -67,7 +62,6 @@ function App() {
     );
     modal.current.close();
 
-    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem(
       'selectedPlaces', 
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
