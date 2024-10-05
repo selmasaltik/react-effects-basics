7bcc8488eeb49215c6442594ad76a28a8c4a399c
selmasaltik <selmasaltiik@gmail.com>
2024-10-05 19:12:14 +0300

    The useCallback Hook

--- 10-place-picker-app/src/App.jsx
+++ 10-place-picker-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -56,17 +56,17 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    // setModalIsOpen(false);
+    setModalIsOpen(false);
 
     localStorage.setItem(
       'selectedPlaces', 
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
